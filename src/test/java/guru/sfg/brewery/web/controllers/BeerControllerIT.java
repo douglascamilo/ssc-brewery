bbcf8cd991324462951e0825e30a39d332fd64d1
Douglas Sousa <douglascamilo@gmail.com>
2023-08-12 12:26:45 -0300

    Url Pattern Matching Configuration

---
+++ src/test/java/guru/sfg/brewery/web/controllers/BeerControllerIT.java
@@ -0,0 +1,34 @@
+package guru.sfg.brewery.web.controllers;
+
+import lombok.SneakyThrows;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.security.test.context.support.WithMockUser;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest
+class BeerControllerIT extends BaseIT {
+
+    @Test
+    @WithMockUser
+    @SneakyThrows
+    void should_retrieve_beers_with_mock_user() {
+        mockMvc.perform(get("/beers/find"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("beers/findBeers"))
+                .andExpect(model().attributeExists("beer"));
+    }
+
+    @Test
+    @SneakyThrows
+    void should_retrieve_beers_with_basic_authentication() {
+        mockMvc.perform(get("/beers/find").with(httpBasic("user", "zika")))
+                .andExpect(status().isOk())
+                .andExpect(view().name("beers/findBeers"))
+                .andExpect(model().attributeExists("beer"));
+    }
+
+}
---
+++ src/test/java/guru/sfg/brewery/web/controllers/IndexControllerIT.java
@@ -0,0 +1,20 @@
+package guru.sfg.brewery.web.controllers;
+
+import lombok.SneakyThrows;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest
+class IndexControllerIT extends BaseIT {
+
+    @Test
+    @SneakyThrows
+    void should_make_get_request_to_index_slash() {
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk());
+    }
+
+}
