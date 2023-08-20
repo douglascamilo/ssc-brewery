ce88b8e0a62030d91e69c5f1fe4dc3d758197b26
Douglas Sousa <douglascamilo@gmail.com>
2023-08-20 17:01:46 -0300

    MD5 Hash and password salt example
    * Too old algorithm and not secure anymore

---
+++ src/test/java/guru/sfg/brewery/web/controllers/PasswordEncodingTest.java
@@ -0,0 +1,26 @@
+package guru.sfg.brewery.web.controllers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.util.DigestUtils;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class PasswordEncodingTest {
+
+    static final String PASSWORD = "password";
+
+    @Test
+    void md5_hashing_example() {
+        var hashedPassword = DigestUtils.md5DigestAsHex(PASSWORD.getBytes());
+
+        assertThat(hashedPassword).isEqualTo("5f4dcc3b5aa765d61d8327deb882cf99");
+    }
+
+    @Test
+    void md5_hashing_example_with_salt() {
+        var salted = PASSWORD + "ThisIsMySALTVALUE";
+        var hashedPassword = DigestUtils.md5DigestAsHex(salted.getBytes());
+
+        assertThat(hashedPassword).isEqualTo("b177e1b7fab112477a6b1754caa9ef06");
+    }
+}
